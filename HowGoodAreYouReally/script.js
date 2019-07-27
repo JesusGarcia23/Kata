8ccbd57f4c817cab468394a82321ccff2d3a7683
= <jacex17@gmail.com>
2019-07-26 21:06:47 -0400

    Add new exercise completed (How good are you really?)

---
+++ HowGoodAreYouReally/script.js
@@ -0,0 +1,6 @@
+function betterThanAverage(classPoints, yourPoints) {
+let averageClass = classPoints.reduce((count, eachGrade) => {
+  return count += eachGrade;
+}, 0);
+ return (averageClass / classPoints.length) < yourPoints;
+}
