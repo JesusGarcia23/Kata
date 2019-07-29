7a4527399e215797d45022bbb8b7b7bb8a9aacdc
= <jacex17@gmail.com>
2019-07-28 22:21:14 -0400

    Fix file

--- HowGoodAreYouReally/script.js
+++ HowGoodAreYouReally/script.js
@@ -2,5 +2,7 @@ function betterThanAverage(classPoints, yourPoints) {
 let averageClass = classPoints.reduce((count, eachGrade) => {
   return count += eachGrade;
 }, 0);
+
  return (averageClass / classPoints.length) < yourPoints;
+
 }
