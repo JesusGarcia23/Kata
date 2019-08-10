e91c3c32c443218a0120aaf2e1041273e3421889
= <jacex17@gmail.com>
2019-08-09 21:07:19 -0400

    fix one line

--- Incrementer/script.js
+++ Incrementer/script.js
@@ -4,7 +4,7 @@ function incrementer(nums) {
     let newArray = []
   for(let i = 0; nums.length > i; i++){
     let actualNum = (nums[i] + i+1);
-    if(actualNum > 10){
+    if(actualNum >= 10){
       newArray.push(actualNum % 10);
     }else{
     newArray.push(actualNum);
