26d4a2dff576be56ef953daa1f933c254ad5acd6
= <jacex17@gmail.com>
2019-09-08 23:09:10 -0400

    Add new excercise completed

---
+++ GrasshopperGradeBook/index.js
@@ -0,0 +1,16 @@
+function getGrade(s1, s2, s3) {
+    let total = (s1 + s2 + s3) / 3;
+    if(total >= 90 && total <= 100){
+    return 'A';
+  }else if(total >= 80 && total < 90){
+    return 'B'
+  }else if(total >= 70 && total < 80){
+  return 'C'
+  }else if(total >= 60 && total < 70){
+  return 'D'
+  }else{
+  return 'F'
+  }
+  }
+  
+  getGrade(48,59,60);
