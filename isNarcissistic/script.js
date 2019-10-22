a74fb4aba5da8c244b24692a6fe812d7fb2c649f
= <jacex17@gmail.com>
2019-10-22 15:24:22 -0400

    Add new exercise

---
+++ isNarcissistic/script.js
@@ -0,0 +1,7 @@
+function isNarcissistic(n){
+
+    return theSum = n.toString().split('').map(Number).reduce((counter, eachNumber) => {
+      return counter + Math.pow(eachNumber, n.toString().length)
+    }, 0) === n
+  
+  }
