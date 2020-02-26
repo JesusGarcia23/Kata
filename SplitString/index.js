6ecc80c96a36c955559ca28bce82e9f3a3551ef1
= <jacex17@gmail.com>
2020-02-26 08:57:20 -0500

    Add new exercise complete

---
+++ SplitString/index.js
@@ -0,0 +1,24 @@
+function solution(str){
+    let myArr = []
+    let flag = true;
+    let i = 0;
+  
+    if(str === ""){
+      return myArr;
+    }
+  else{
+    while(flag) {
+  
+      if(i+1 === str.length) {
+        myArr.push(`${str.charAt(i)}_`)
+      }else{
+        myArr.push(`${str.charAt(i)}${str.charAt(i+1)}`)
+      }
+  
+      i += 2
+  
+      if(i >= str.length) {
+        flag = false;
+      }
+    }
+  }
