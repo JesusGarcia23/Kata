dbf6da84eac063ab3f15a69321756955c2275de8
= <jacex17@gmail.com>
2020-06-15 21:39:11 -0700

    Add new exercise completed

---
+++ CreateTheRooms/index.js
@@ -0,0 +1,20 @@
+// Escape the room
+// You are creating an "Escape the room" game.
+// The first step is to create a hash table called rooms that contains all of the rooms of the game.
+// There should be at least 3 rooms inside it, each being a hash table with at least three properties
+// (e.g. name, description, completed).
+
+// Add rooms here
+var rooms = [{
+"name": "",
+"description": "",
+"completed": ""
+},
+{"name": "",
+"description" : "",
+"completed": ""
+},
+{"name": "",
+"description" : "",
+"completed": ""
+}]
