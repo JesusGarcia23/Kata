22fda9ff21944af2879127b9fb9144b75a4e62a3
= <jacex17@gmail.com>
2021-02-08 17:54:41 -0800

    Add new exercise completed

--- SimpleLetterRemoval/index.js
+++ SimpleLetterRemoval/index.js
@@ -3,6 +3,24 @@
 // - first remove all letter 'a', followed by letter 'b', then 'c', etc...
 // - remove the leftmost character first.
 
+
+//second version
+
+function solve(s, k){
+  const abc = "abcdefghijklmnopqrstuvwxyz"
+  
+  for (let i=0; i<abc.length; i+=1) {
+    while(s.includes(abc[i]) && k > 0) {
+      s = s.replace(abc[i], "")
+      k -= 1
+    }
+  }
+  
+  return s
+}
+
+//  first version
+
 function solve(s, k){
 
     let actualLetter = 97
